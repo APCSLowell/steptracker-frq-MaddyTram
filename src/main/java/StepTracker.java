0363019a63e780610117a243969af3d758d3afc9
MaddyTram <116767768+MaddyTram@users.noreply.github.com>
2025-04-23 11:04:47 -0700

    Update StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -2,4 +2,32 @@ import java.util.ArrayList;
 public class StepTracker
 {
  /* to be implemented here */
+ private int minStep, totalStep;
+ private int totalDays, actDays;
+
+ public StepTracker() {
+  minStep = goal;
+  totalStep = 0;
+  totalDays = 0;
+  actDays = 0;
+ }
+
+ public void addDailySteps(int steps) {
+  totalStep += steps;
+  totalDays++;
+  if(steps >= minStep) {
+   actDays++;
+  }
+ }
+ public int activeDays() {
+  return actDays;
+ }
+
+ public double averageSteps() {
+  if(totalDays == 0) {
+   return 0.0;
+  } else {
+   return (double) totalStep/totalDays;
+  }
+ }
 } 
